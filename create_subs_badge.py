4589d54fe193d50bf88a27401556ea986001b786
Silleellie <antonio.silletti@gmail.com>
2023-02-20 12:44:39 +0100

    added check for invalid readme path

--- create_subs_badge.py
+++ create_subs_badge.py
@@ -1,9 +1,13 @@
+import os.path
 import re
 import sys
 
 README_PATH = sys.argv[1]
 NUMERIC_SCORE = sys.argv[2]
 BADGE_COLOR = sys.argv[3]
+
+if not os.path.isfile(README_PATH):
+    raise FileNotFoundError(f"README.md path is wrong, no file can be located at {README_PATH}")
 
 with open(README_PATH, "r", encoding="utf8") as f:
     content = f.read()
